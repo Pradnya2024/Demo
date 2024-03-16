faec872be7e4dd3984ddfee3ed20583230eb2686
yashthakur83 <yashthakur83600@gmail.com>
2024-03-16 08:25:37 +0530

    Emp

---
+++ Project/src/Emp.java
@@ -0,0 +1,31 @@
+ 
+
+
+
+
+public class Emp {
+     int id = 12;
+	 String Name = "Pradnya" ;
+	 double salary  = 25000;
+	 String Address; 
+	 
+	 
+	 void display (){
+		 System.out.println(id );
+		 System.out.println("Name" );
+		 System.out.println( " salary "  );
+		 System.out.println(  " Address " );
+		 
+		 
+	 }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		Emp ob = new Emp ();
+		ob.display();
+		
+		
+	}
+
+}
