d52dabec3db552f94721fce9aa556672d3b0ee6a
yashthakur83 <yashthakur83600@gmail.com>
2024-03-16 08:24:31 +0530

    overriding

---
+++ Project/src/overriding.java
@@ -0,0 +1,20 @@
+
+class B { 
+	void add(int a , int b){
+		
+		System.out.println(a+b );
+	}
+	
+}
+
+public class overriding {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	B ob = new B ();
+	ob.add(10,30);
+	
+
+}
+}
+
